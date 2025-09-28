617b1956e9cea8d5b97bc9a0da03c7483d26698d
victoriaUNI <victoria.00000851663@unicap.br>
2025-09-28 19:35:18 -0300

    bleh

--- app/database.py
+++ app/database.py
@@ -2,20 +2,20 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.config import settings
 
-# URL do banco vinda do config.py
+# Pega a URL do banco do config.py
 DATABASE_URL = settings.DATABASE_URL
 
 # Cria engine de conexão
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
-# Cria sessão para interagir com o banco
+# Cria sessão
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Base para os models (ex.: Usuario, Atividade etc.)
+# Base para os models herdarem
 Base = declarative_base()
 
 
-# Dependência do FastAPI para injetar sessão no endpoint
+# Dependency para usar nos endpoints
 def get_db():
     db = SessionLocal()
     try:
